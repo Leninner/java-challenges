e4f6c375ccf3d1b27f2f18113492a5e87e6692e0
Leninner <mazabandalenin180@gmail.com>
2021-12-19 21:53:47 -0500

    Update

--- Hello.java
+++ Hello.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 class Hello {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)){
-          System.out.println("Hello, World!");
-          System.out.println("What is your name?");
-          String name = scanner.nextLine();
-          System.out.println("Hello, " + name + "!");
+            System.out.println("Hello, World!");
+            System.out.println("What is your name?");
+            String name = scanner.nextLine();
+            System.out.println("Hello, " + name + "!");
         }
         catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
