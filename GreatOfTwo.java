7d3042dbb19e8ec0ac15c6a1cf7283eae970cb6e
Leninner <mazabandalenin180@gmail.com>
2021-12-23 20:19:38 -0500

    JOptionPane

---
+++ GreatOfTwo.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class GreatOfTwo {
+  public static void main(String[] args) {
+    int numberOne = Integer.parseInt(JOptionPane.showInputDialog("Digita un número entero"));
+    int numberTwo = Integer.parseInt(JOptionPane.showInputDialog("Digita otro número entero"));
+
+    if (numberOne == numberTwo){
+      JOptionPane.showMessageDialog(null, "Los números son iguales");
+    }
+    else if (numberOne > numberTwo) {
+      JOptionPane.showMessageDialog(null, "El número " + numberOne + " es mayor que el número " + numberTwo);
+    } else {
+      JOptionPane.showMessageDialog(null, "El número " + numberTwo + " es mayor que el número " + numberOne);
+    }
+  }
+}
