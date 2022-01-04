c712e0e850b40432ba18ac9e738cbcf91f9cf947
Leninner <mazabandalenin180@gmail.com>
2022-01-03 22:12:55 -0500

    Update

---
+++ salaryWeekly.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class salaryWeekly {
+  public static void main(String[] args) {
+    try(Scanner scan = new Scanner(System.in)) {
+      System.out.print("Ingresa las horas semanales trabajadas: ");
+      int horas = scan.nextInt();
+
+      if(horas <=40){
+        System.out.println("El salario semanal del trabajador es: " + (horas * 16));
+      } else {
+        System.out.println("El salario seamanal del trabajador es: " + ((40*16) + ((horas-40) * 20)));
+      }
+    }
+  }
+}
