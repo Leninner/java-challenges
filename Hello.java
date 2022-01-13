f2526a20a424d5383b1272fc4c7c4307d30235cb
Leninner <mazabandalenin180@gmail.com>
2022-01-12 19:30:24 -0500

    Update

--- Hello.java
+++ Hello.java
@@ -4,8 +4,11 @@ class Hello {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)){
             System.out.println("Hello, World!");
+
             System.out.println("What is your name?");
+
             String name = scanner.nextLine();
+            
             System.out.println("Hello, " + name + "!");
         }
         catch (Exception e) {
