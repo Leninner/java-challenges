ade553e26c6c9f5e9efbcaa56210fe11ee3be27b
Leninner <mazabandalenin180@gmail.com>
2022-01-13 19:01:44 -0500

    Updtae

---
+++ Promedio.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Promedio {
+  public static void main(String[] args) {
+    try(Scanner scan = new Scanner(System.in)){
+      System.out.println("Ingresa las notas del estudiante: ");
+
+      float gradeOne = scan.nextFloat();
+      float gradeTwo = scan.nextFloat();
+      float gradeThree = scan.nextFloat();
+      float gradeFour = scan.nextFloat();
+
+      float promedio = (gradeOne + gradeTwo + gradeThree + gradeFour) / 4;
+      System.out.println("El promedio del estudiante es: " + promedio);
+    }
+  }
+}
