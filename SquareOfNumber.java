403b1f86d71411efd30f247b4a493c10180beee8
Leninner <mazabandalenin180@gmail.com>
2022-01-18 21:54:03 -0500

    Update project

---
+++ SquareOfNumber.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class SquareOfNumber {
+  public static void main(String[] args) {
+    double number = Double.parseDouble(JOptionPane.showInputDialog("Digite un número"));
+    
+    while(number > 0){
+      JOptionPane.showMessageDialog(null, "El cuadrado de tu número es: " + (Math.pow(number, 2)));
+      number = Double.parseDouble(JOptionPane.showInputDialog("Digite un número"));
+    }
+  }
+}
