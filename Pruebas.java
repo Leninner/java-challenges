04a82b6ecfb378004192c08abe65ad9c1734e610
Leninner <mazabandalenin180@gmail.com>
2022-02-16 22:18:19 -0500

    Add update

--- Pruebas.java
+++ Pruebas.java
@@ -2,19 +2,52 @@ import java.util.Scanner;
 
 public class Pruebas {
   public static void main(String[] args) {
-    try (Scanner scan = new Scanner(System.in)) {
-      System.out.println("Introduce el primer número: ");
-      int numberOne = scan.nextInt();
-      System.out.println("Introduce el segundo número: ");
-      int numberTwo = scan.nextInt();
+    Scanner scan = new Scanner(System.in);
+    int saldo = 1000;
+    System.out.println("Ingresa tu nombre");
+    String nameUser = scan.nextLine();
+    int retiro = 0;
+    int deposito = 0;
 
-      int result = numberOne % numberTwo;
+    System.out.println("¿Qué quiéres hacer hoy? " + nameUser + " Retiro(R), Depósito(D), Consultar(C)");
 
-      if (result == 0) {
-        System.out.println("El número " + numberOne + " es múltiplo de " + numberTwo);
-      } else {
-        System.out.println("Vuelve a la escuela por favor");
-      }
+    char option = scan.nextLine().toUpperCase().charAt(0);
+
+    switch (option) {
+      case 'R':
+
+        System.out.println("Ingresa la cantidad de dinero que quieres");
+        retiro = scan.nextInt();
+        saldo -= retiro;
+
+        if (retiro <= saldo) {
+          System.out.println("Haz recibido " + retiro);
+          System.out.println("Te queda " + saldo);
+        } else {
+          System.out.println("Eres pobre, no tienes esa cantidad de dinero");
+        }
+
+        break;
+
+      case 'D':
+        System.out.println("Ingresa cuánto dinero quiéres depositar");
+        deposito = scan.nextInt();
+
+        if (deposito > 0) {
+          System.out.println("Tu saldo actual es " + saldo);
+          saldo += deposito;
+          System.out.println("Tu saldo actual con el depósito es " + saldo);
+        } else {
+          System.out.println("Estás pendejo");
+        }
+        break;
+
+      case 'C':
+        System.out.println("Tu saldo actual es: " + saldo);
+        break;
+
+      default:
+        System.out.println("Error 404, cómprate una vida");
     }
   }
 }
