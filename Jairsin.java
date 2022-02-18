d524d44a7c191817f0833496de39c2dcfe4e87f8
Leninner <mazabandalenin180@gmail.com>
2022-02-18 13:05:32 -0500

    Update

---
+++ Jairsin.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Jairsin {
+  public static void main(String[] args) {
+    try (Scanner entrada = new Scanner(System.in)) {
+      Boolean isOpen = true;
+
+      int number;
+
+      while (isOpen) {
+        System.out.println("Ingresa un número");
+        number = entrada.nextInt();
+
+        switch (number) {
+          case 10:
+            System.out.println("Bien");
+            break;
+
+          case 50:
+            System.out.println("Cool");
+            break;
+
+          case 45:
+            System.out.println("Bieeen");
+            isOpen = false;
+            break;
+        }
+      }
+
+    } catch (Exception e) {
+      System.out.println("Eres" + e);
+    }
+
+  }
+}
