dea9f4daa7fb43bc988d16eeec8e98fa4d14b985
Leninner <mazabandalenin180@gmail.com>
2022-02-23 12:44:21 -0500

    Update

--- BubbleSort.java
+++ BubbleSort.java
@@ -15,12 +15,16 @@ public class BubbleSort {
       numbers[i] = scan.nextInt();
     }
 
+    // Bubble sort - Mayor a menor
+
+    // O(n) => O(n^2)
+
     for (int i = 0; i < numbers.length - 1; i++) {
       for (int j = 0; j < numbers.length - 1; j++) {
-        if (numbers[j] > numbers[j + 1]) {
-          aux = numbers[j];
-          numbers[j] = numbers[j + 1];
-          numbers[j + 1] = aux;
+        if (numbers[j] < numbers[j + 1]) {
+          aux = numbers[j + 1];
+          numbers[j + 1] = numbers[j];
+          numbers[j] = aux;
         }
       }
     }
