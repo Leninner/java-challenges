d625390052dc8c8d4f82d58cb3b38be9bb1de6d2
Leninner <mazabandalenin180@gmail.com>
2022-03-11 11:17:11 -0500

    Update

---
+++ Matrices.java
@@ -0,0 +1,60 @@
+public class Matrices {
+  public static void main(String[] args) {
+    // int matrix[][] = new int[2][3];
+    // matrix[0][0] = 0;
+    // matrix[0][1] = 0;
+    // matrix[0][2] = 0;
+    // matrix[1][0] = 0;
+    // matrix[1][1] = 0;
+    // matrix[1][2] = 0;
+
+    // String result = "";
+
+    // for (int i = 0; i < matrix.length; i++) {
+    // for (int j = 0; j < matrix[i].length; j++) {
+    // result += matrix[i][j] + " ";
+    // }
+    // result += "\n";
+    // }
+
+    // // F == C
+    // Boolean isSimetric = true;
+
+    // if (matrix.length == matrix[0].length) {
+    // int i = 0, j = 0;
+
+    // while (i < matrix.length && isSimetric == true) {
+    // j = 0;
+    // while (j < i && isSimetric == true) {
+    // if (matrix[i][j] != matrix[j][i]) {
+    // isSimetric = false;
+    // }
+    // j++;
+    // }
+    // i++;
+    // }
+    // } else {
+    // isSimetric = false;
+    // }
+
+    // if (isSimetric) {
+    // System.out.println("La matriz es simétrica");
+    // } else {
+    // System.out.println("No es simétrica");
+    // }
+
+    // JOptionPane.showMessageDialog(null, result);
+
+    // i++ => i = i + 1
+    // i = i + 2
+    int sumaTotal = 0;
+
+    for (int i = 0; i < 10; i++) {
+      //
+      System.out.println(sumaTotal + i);
+      sumaTotal += i;
+    }
+
+    // Promedio
+  }
+}
