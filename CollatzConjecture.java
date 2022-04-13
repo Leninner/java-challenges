3ebf7be8a900e62e83e5133e9e4297f78ea23626
Leninner <mazabandalenin180@gmail.com>
2022-04-12 21:32:39 -0500

    Added Collatz Conjeture

---
+++ CollatzConjecture.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CollatzConjecture {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.print("Ingresa un número para generar la conjetura de collatz: ");
+    int number = scan.nextInt();
+    scan.close();
+
+    while (number > 1) {
+      if (number % 2 == 0) {
+        number /= 2;
+      } else {
+        number = (number * 3) + 1;
+      }
+
+      System.out.print(number + " - ");
+    }
+  }
+}
