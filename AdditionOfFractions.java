7dc3e0ef8f44fd59b04415e0a967edad16ced743
Leninner <mazabandalenin180@gmail.com>
2022-04-12 19:10:44 -0500

    Added Euler Number solution

---
+++ AdditionOfFractions.java
@@ -0,0 +1,16 @@
+public class AdditionOfFractions {
+  public static void main(String[] args) {
+    int base = 2, power = 1;
+    double fraction = 0.5, accumulativeSum = 0;
+
+    System.out.println("Potencia \t Fracción \t Suma");
+    System.out.println("--------------------------------------------");
+
+    while (fraction >= 0.000001) {
+      accumulativeSum += fraction;
+      System.out.printf("%d \t\t %.5f \t %.5f\n", power, fraction, accumulativeSum);
+      power++;
+      fraction = (1 / Math.pow(base, power));
+    }
+  }
+}
--- Asterisks.java
+++ Asterisks.java
@@ -56,5 +56,25 @@ public class Asterisks {
 
       System.out.println("");
     }
+
+    System.out.println("Método para imprimir un un hexágono");
+
+    System.out.println("");
+
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        if (i == 0 || i == height - 1) {
+          System.out.print(" *");
+        } else {
+          if (j == 0 || j == width - 1) {
+            System.out.print(" *");
+          } else {
+            System.out.print("  ");
+          }
+        }
+      }
+
+      System.out.println("");
+    }
   }
 }
