6fa77735a8f977e068200403a1a6695a6443538d
leninner <mazabandalenin180@gmail.com>
2022-05-20 22:32:21 -0500

    Added Strings

---
+++ Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+  public static void main(String[] args) {
+    String cadena = "Lenin | Matemáticas | Física | Química | Biología | Historia | Geografía | Artes | Ciencias | Ingles | Español | Francés | Italiano";
+
+    String[] materias = cadena.split("\\|");
+
+    for (int i = 0; i < materias.length; i++) {
+      System.out.println(materias[i]);
+    }
+  }
+}
