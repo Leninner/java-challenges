1717bf71a2c3350f23249922149b1108f914a6a9
leninner <mazabandalenin180@gmail.com>
2022-06-04 19:35:28 -0500

    Learning Java

--- POOChallenge/Main.java
+++ POOChallenge/Main.java
@@ -3,51 +3,37 @@ package POOChallenge;
 import java.util.Scanner;
 
 public class Main {
-    public static int getCheapestVehicleIndex(Vehicle[] vehicles) {
-        int cheapestVehicleIndex = 0;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingrese el número de atletas: ");
+        int numberAtletas = scan.nextInt();
 
-        for (int i = 1; i < vehicles.length; i++) {
-            if (vehicles[i].getVehiclePrice() < vehicles[cheapestVehicleIndex].getVehiclePrice()) {
-                cheapestVehicleIndex = i;
+        Atletismo[] atletas = new Atletismo[numberAtletas];
+
+        for (int i = 0; i < numberAtletas; i++) {
+            System.out.println("Ingrese el número de atleta: ");
+            System.out.print("-> ");
+            int numberAtleta = scan.nextInt();
+
+            System.out.println("Ingrese el nombre del atleta: ");
+            System.out.print("-> ");
+            String nameAtleta = scan.next();
+
+            System.out.println("Ingrese el tiempo del atleta: ");
+            System.out.print("-> ");
+            double timeAtleta = scan.nextDouble();
+
+            atletas[i] = new Atletismo(numberAtleta, nameAtleta, timeAtleta);
+        }
+
+        Atletismo winnerAtleta = atletas[0];
+
+        for (int i = 0; i < numberAtletas; i++) {
+            if (atletas[i].getTimeAtleta() < winnerAtleta.getTimeAtleta()) {
+                winnerAtleta = atletas[i];
             }
         }
 
-        return cheapestVehicleIndex;
-    }
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String vehicleBrand;
-        String vehicleModel;
-        double vehiclePrice;
-
-        System.out.println("Digita la cantidad de vehiculos que deseas agregar: ");
-        System.out.print("-> ");
-        int vehiclesQuantity = scan.nextInt();
-
-        Vehicle[] vehicles = new Vehicle[vehiclesQuantity];
-
-        System.out.println("Ingresa los datos de los vehiculos: ");
-
-        for (int i = 0; i < vehicles.length; i++) {
-            System.out.println("\nDigita la marca del vehiculo: ");
-            System.out.print("-> ");
-            vehicleBrand = scan.next();
-
-            System.out.println("\nDigita el modelo del vehiculo: ");
-            System.out.print("-> ");
-            vehicleModel = scan.next();
-
-            System.out.println("\nDigita el precio del vehiculo: ");
-            System.out.print("-> ");
-            vehiclePrice = scan.nextDouble();
-
-            vehicles[i] = new Vehicle(vehicleBrand, vehicleModel, vehiclePrice);
-        }
-
-        scan.close();
-
-        System.out.println("\nEl coche mas barato es: ");
-        System.out.println(vehicles[getCheapestVehicleIndex(vehicles)].showVehicleInfo());
+        System.out.println("El atleta ganador de la carrera es: " + winnerAtleta.getAtletaInfo());
     }
 }
